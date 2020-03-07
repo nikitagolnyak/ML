be21214978dc06245eecc795ea88d13a56b16bb2
Anna Kapranova <annakapranova77@gmail.com>
2020-03-07 12:11:10 +0300

    D with h opt + convergence

--- cf/D.py
+++ cf/D.py
@@ -9,56 +9,45 @@ def predict(row, W):
     return y_pred
 
 
-# def stochastic_gradient_step_v(row, y_actual, W):
-#     y_pred = predict(row, W)
-#     error = y_pred - y_actual
-#     gr = [0] * len(W)
-#     dx = 0.0
-#     h = 0.0
-#     for i in range(len(row)):
-#         gr_value = row[i] * error * 2
-#         gr[i] += gr_value
-#         dx += row[i] * gr_value
-#         # W[i] = W[i] - l_rate * error * row[i]
-#     if dx != 0:
-#         curr_h = error / dx
-#         if h < curr_h:
-#             h = curr_h
-#     if h == 0:
-#         return W
-#     for i in range(len(W)):
-#         W[i] = W[i] - h * gr[i]
-#     return W
-
-
-def stochastic_gradient_step_v(row, y_actual, W):
+def stochastic_gradient_step_v(row, y_actual, W, Q, lam):
     y_pred = predict(row, W)
-    gr = [0] * len(W)
-    h = 0.0
-    value = y_pred - y_actual
-    gr_value = value * 2
-    dx = 0.0
-    for j in range(len(row)):
-        j_gr_value = row[j] * gr_value
-        gr[j] += j_gr_value
-        dx += row[j] * j_gr_value
-    if dx != 0:
-        curr_h = value / dx
-        if h < curr_h:
-            h = curr_h
-    if h == 0:
-        return W
+    error = y_pred - y_actual
+    Q_new = lam * error + (1 - lam) * Q
+    if abs(Q_new - Q) < 0.000001:
+        return W, Q, True
+    l_rate = 0.0
+    W_cur = [0] * len(W)
+    dx = 0
     for i in range(len(W)):
-        W[i] = W[i] - h * gr[i]
-    return W
+        W_cur[i] = row[i] * error
+        dx += row[i] * W_cur[i]
+    if dx != 0:
+        if error / dx > l_rate:
+            l_rate = error / dx
+    if l_rate == 0:
+        return W, Q_new, False
+    for j in range(len(W)):
+        W[j] = W[j] - l_rate * W_cur[j]
+    return W, Q, False
+
+
+def init_loss(X, Y, W):
+    Q = 0.0
+    size = len(X)
+    for i in range(size):
+        y_pred = predict(X[i], W)
+        Q += y_pred - Y[i]
+    return Q / size
 
 
 def compute_coefficients(X, Y, n_epoch, W):
     iter_num = 0
-    alpha = 1 / len(X)
+    lam = 1 / len(X)
+    Q = init_loss(X, Y, W)
     while iter_num < n_epoch:
         random_ind = randint(0, len(X) - 1)
-        W = stochastic_gradient_step_v(X[random_ind], Y[random_ind], W)
+        W, Q, stop = stochastic_gradient_step_v(X[random_ind], Y[random_ind], W, Q, lam)
+        if stop: return W
         iter_num += 1
     return W
 
